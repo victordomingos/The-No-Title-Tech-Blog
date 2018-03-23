27b561f3f34dca556f26ba3a6674362dfc76c8f9
Victor Domingos <victor@MacBook-Pro-de-Victor.local>
2018-03-23 01:33:39 +0000

    Updated some settings

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Victor Domingos'
-SITENAME = u'The <strong>No Title<small><sup>&reg;</sup></small></strong>Tech Blog'
+SITENAME = u'The <strong>No Title<small><sup>&reg;</sup></small></strong> Tech Blog'
 SITEURL = 'http://victordomingos.com/no-title'
 BIO = 'Tales of a stranger in antigravity land.'
 PATH = 'content'
@@ -24,17 +24,13 @@ DISPLAY_CATEGORIES_ON_MENU = True
 DISPLAY_PAGES_ON_MENU = True
 
 # Blogroll
+
+"""
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('facebook', 'https://www.facebook.com/escritorvictordomingos/'),
-          ('twitter', 'https://twitter.com/victordomingos'),
-          ('linkedin', 'https://linkedin.com/in/victordomingos'),
-          ('github', 'https://github.com/victordomingos'),)
-
+"""
 
 STATIC_PATHS = ['images',]
 PAGE_PATHS = ['pages',]
@@ -46,11 +42,43 @@ RELATIVE_URLS = True
 
 
 #PROFILE_IMAGE = 'avatar.jpg'
-#THEME="themes/hyde"
-
-THEME="themes/pelican-alchemy/alchemy"
 
 
-#DISQUS_SITENAME = ""
+THEME = "themes/pelican-alchemy/alchemy"
+SITESUBTITLE = 'Tales of an exploration on antigravity and other potentialy unrelated matters'
+#SITEIMAGE = '/images/profile.png'  # Image that appears in the header
+
+DESCRIPTION = 'A personal blog about learning programming technologies, including programming Python, tkinter, html, css, sql, flask, and other stuff.'
 
 
+# Social widget
+ICONS = (('facebook', 'https://www.facebook.com/escritorvictordomingos/'),
+         ('twitter', 'https://twitter.com/victordomingos'),
+         ('linkedin', 'https://linkedin.com/in/victordomingos'),
+         ('github', 'https://github.com/victordomingos'),
+         ('stack-overflow', 'https://stackoverflow.com/users/6167478/victor-domingos'),)
+
+TAGS_SAVE_AS = ''
+TAG_SAVE_AS = ''
+
+LOCALE = "en_GB"
+
+#PYGMENTS_STYLE = ""
+
+# code blocks with line numbers
+PYGMENTS_RST_OPTIONS = {'linenos': 'table'}
+
+HIDE_AUTHORS = True
+
+# https://realfavicongenerator.net/blog/new-favicon-package-less-is-more/
+#RFG_FAVICONS =
+
+TYPOGRIFY = True
+
+SUMMARY_MAX_LENGTH = 50
+
+PAGE_ORDER_BY = 'reversed-basename'
+
+WITH_FUTURE_DATES = False
+YEAR_ARCHIVE_SAVE_AS = 'articles/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'articles/{date:%Y}/{date:%b}/index.html'
