2e80baba03721e72fed3ab3a3cbbc341f6c7407c
Retina13 <editor.arcosonline@gmail.com>
2018-03-26 12:37:01 +0100

    ...

--- src/get_website_source.sh
+++ src/get_website_source.sh
@@ -3,7 +3,6 @@ echo " "
 echo "Obtaining current source from Git repository..."
 git clone https://github.com/victordomingos/The-No-Title-Tech-Blog.git
 cd The-No-Title-Tech-Blog
-git checkout src
 cd src
 pelican content
 ls -lhosa
