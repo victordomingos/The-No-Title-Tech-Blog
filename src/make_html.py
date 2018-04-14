910edc302539623398b156a425ba8c9b7982f5bc
Victor Domingos <victordomingos@users.noreply.github.com>
2018-04-14 10:31:04 +0100

    ...

--- src/make_html.py
+++ src/make_html.py
@@ -9,7 +9,7 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
-settings_filepath = os.path.expanduser(dname+"/pelicanconf.py")
+settings_filepath = os.path.expanduser(dname+"/publishconf.py")
 settings = read_settings(settings_filepath)
 pelican = Pelican(settings)
 pelican.run()
