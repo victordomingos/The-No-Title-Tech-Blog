24676e74b8b7acab64f5c962f79d460e285e47e5
Victor Domingos <victordomingos@users.noreply.github.com>
2018-06-30 02:49:11 +0100

    …

--- src/make_html.py
+++ src/make_html.py
@@ -4,12 +4,14 @@ import os
 from pelican import Pelican
 from pelican.settings import read_settings
 
-
+print '\nReading the publish settings file.\n'
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
 settings_filepath = os.path.expanduser(dname+"/publishconf.py")
 settings = read_settings(settings_filepath)
+
+print 'Building the website with Pelican...\n'
 pelican = Pelican(settings)
 pelican.run()
