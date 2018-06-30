6823853d837d3d158b458a2773f30712c7abc6cc
Victor Domingos <victordomingos@users.noreply.github.com>
2018-06-30 02:55:20 +0100

    small changes in scripts

--- src/direct_upload.py
+++ src/direct_upload.py
@@ -5,11 +5,13 @@ from ftpsync.synchronizers import UploadSynchronizer
 
 from ftpsetupfile import *
 
+print('\n\nOpening an FTP connection for the upload...\n')
 
 local = FsTarget(LOCAL_DIR)
 remote = FtpTarget(REMOTE_DIR, REMOTE_HOST, username=FTP_USERNAME, password=FTP_PASSWD)
-opts = {"force": True, "delete_unmatched": True, "verbose": 3}
+opts = {"force": True, "delete_unmatched": True, "verbose": 2}
 
 s = UploadSynchronizer(local, remote, opts)
 s.run()
 
+print('\n\nUpload complete!')
