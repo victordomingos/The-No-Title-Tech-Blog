9f1f79232cc672febbcd3d948d0b3a2a8d7e0667
victordomingos <editor.arcosonline@gmail.com>
2018-11-15 20:41:50 +0000

    ...

--- src/requirements_for_haiku_install_from_HaikuDepot.sh
+++ src/requirements_for_haiku_install_from_HaikuDepot.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
+pkgman refresh;
 pkgman install python3 pip_python3 beautifulsoup4_python3 chardet_python3 docutils_python3 idna_python3 pillow_python3 requests_python3 urllib3_python3 markdown_python3;
 echo "Done!"
