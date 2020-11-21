a7b4c661a7de69b46f07b96b60465d5f5634a036
victordomingos <editor.arcosonline@gmail.com>
2020-11-21 12:34:45 +0000

    updated script for macos big sur

--- src/replace_pillow.sh
+++ src/replace_pillow.sh
@@ -1,6 +1,9 @@
 brew install libjpeg-turbo
+brew install zlib
 export LDFLAGS="-L/usr/local/opt/jpeg-turbo/lib"
 export CPPFLAGS="-I/usr/local/opt/jpeg-turbo/include"
+export LDFLAGS="-L/usr/local/opt/zlib/lib"
+export CPPFLAGS="-I/usr/local/opt/zlib/include"
 export CPATH=`xcrun --show-sdk-path`/usr/include
 pip3.8 uninstall pillow; 
 CFLAGS="${CFLAGS} -mavx2" pip3.8 install --upgrade --no-cache-dir --force-reinstall --no-binary :all: --compile pillow-simd
